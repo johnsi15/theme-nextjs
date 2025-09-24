a6ac20f51b4643e3a44e0a5ffe91794217aada9d
John Serrano <andrey_201015@hotmail.com>
2025-09-24 11:13:23 -0500

    Añadir soporte para extractos en páginas

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 add_theme_support('post-thumbnails');
 
+function add_excerpt_to_pages() {
+  add_post_type_support('page', 'excerpt');
+}
+
+add_action('init', 'add_excerpt_to_pages');
+
 /**
  * Registers new menus
  *
